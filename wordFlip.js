9c008b62fb74b90bd2d0e3bcce6f142bcc78fda0
AaronGusa <gus21002@byui.edu>
2025-02-25 14:09:15 -0700

    Lots of CSS for homepage

---
+++ wordFlip.js
@@ -0,0 +1,55 @@
+// const textList = ["Web Design", "Software Development", "Graphic Design", "Photography"];
+
+// const cycle = document.querySelector("#cycle");
+// let i = 0;
+// const cycleText = () => {
+//   cycle.innerHTML = textList[i];
+//   i = ++i % textList.length;
+//   cycle.style.opacity = 1;
+  
+// };
+// cycleText();
+// setInterval(cycleText, 5000);
+
+
+
+
+
+
+// let words = document.getElementsByClassName('word');
+// let wordArray = [];
+// console.log(words);
+
+// let currentWord = 0;
+// words[currentWord].style.opacity = 1;
+
+// function getWords() {
+//     for (let i  = 0; words.length > i; i++) {
+//         wordArray.push(words[i].innerHTML);
+//     }
+// }
+
+// getWords();
+
+// console.log(wordArray);
+// function changeWord() {
+//     let cw = wordArray[currentWord];
+//     if (currentWord+1 !== wordArray.length) {
+//         currentWord++;
+//         let nw = wordArray[currentWord];
+        
+//     }
+// }
+
+// // function changeWord() {
+// //   let cw = wordArray[currentWord];
+// //   let nw = currentWord == words.length-1 ? words[0] : words[currentWord+1];
+// //   cw.style.opacity = 0;
+// //   setTimeout(function() {
+// //     nw.style.opacity = 1;
+// //     currentWord = (currentWord == words.length-1) ? 0 : currentWord+1;
+// //   }, 800);
+// // }
+
+
+// setInterval(changeWord, 4000);
