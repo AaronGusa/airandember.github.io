5204e006f30c31dd2469f1440c5022846489969b
Goosa2025 <gus21002@byui.edu>
2025-03-03 15:06:28 -0700

    Contact updates

--- contact.js
+++ contact.js
@@ -1,19 +1,38 @@
-function sendMail() { document.getElementById('contactForm').addEventListener('submit', function(event) { event.preventDefault();
-    const honeypot = document.querySelector('[name="contact_me_by_email"]').checked;
-     if (honeypot) { 
-       console.log('Spam bot detected');
-        return;
-     } 
-     const name = document.getElementById('name').value;
-     const famName = document.getElementById('famName').value;
-     const subject = `RING A DING: You've got site mail!`;
-     const email = document.getElementById('email').value;
-     const message = document.getElementById('message').value;
-     const emailContent = `Name: ${name}\nFamily:${famName}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`;
-     const mailtoLink = `mailto:drgoose@drgoose.ink?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(emailContent)}`;
-     window.location.href = mailtoLink; 
-     console.log(emailContent);
-   }
- );
- }
- 
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const honeypot = document.querySelector('[name="contact_me_back"]').checked;
+  if (honeypot) {
+      console.log('Spam bot detected');
+      return;
+  }
+
+  const name = document.getElementById('name').value;
+  const companyName = document.getElementById('company_name').value;
+  const subject = `RING A DING: You've got Air& Ember site contact e-mail!`;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  const emailContent = {
+      name,
+      companyName,
+      subject,
+      email,
+      message
+  };
+
+  fetch('https://aaronandemberbe.onrender.com/contact', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(emailContent)
+  })
+  .then(response => response.json())
+  .then(data => {
+      console.log('Email sent:', data);
+  })
+  .catch(error => {
+      console.error('Error:', error);
+  });
+})
