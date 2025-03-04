ebe203e7d9d10315f175e413b05bd24447c66615
Goosa2025 <gus21002@byui.edu>
2025-03-03 17:47:28 -0700

    Output updates

--- contact.js
+++ contact.js
@@ -30,7 +30,8 @@ document.getElementById('contactForm').addEventListener('submit', function(event
   })
   .then(response => response.json())
   .then(data => {
-      console.log('Email sent:', data);
+      console.log('Email sent:');
+      console.log(data);
       // Display success message
       alert('Thank you for your message! We will get back to you soon.');
   })
